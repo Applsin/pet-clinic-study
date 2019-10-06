b3d6c11fadc5a3348819b1ac03f589f74ec9be9b
FluffyDumbo <javatechno@mail.ru>
2019-10-06 21:12:33 +0300

    init project

---
+++ src/main/java/alex/petclinic/petclinicstudy/PetclinicStudyApplication.java
@@ -0,0 +1,13 @@
+package alex.petclinic.petclinicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicStudyApplication.class, args);
+    }
+
+}
